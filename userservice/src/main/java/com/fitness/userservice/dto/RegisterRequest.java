fb3768864235a2fe98e1884f4218d43f837aba5d
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-08-30 14:32:48 +0530

    adding register request dto

---
+++ userservice/src/main/java/com/fitness/userservice/dto/RegisterRequest.java
@@ -0,0 +1,19 @@
+package com.fitness.userservice.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record RegisterRequest(
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    String email,
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must have atleast 6 characters")
+    String password,
+    
+    String firstName,
+    String lastName
+) {
+} 
