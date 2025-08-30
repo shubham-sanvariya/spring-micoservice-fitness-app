d942803929e7b2070b67a66438bae9442a91373b
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-08-30 14:06:02 +0530

    adding user repo

---
+++ userservice/src/main/java/com/fitness/userservice/repo/UserRepo.java
@@ -0,0 +1,9 @@
+package com.fitness.userservice.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fitness.userservice.model.User;
+
+public interface UserRepo extends JpaRepository<User, String>{
+    
+}
