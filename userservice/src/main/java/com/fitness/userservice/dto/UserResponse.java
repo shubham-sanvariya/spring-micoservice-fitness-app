9612c6414fb3fda16f54604b489b03354fd07772
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-08-30 13:57:07 +0530

    adding user response dto

---
+++ userservice/src/main/java/com/fitness/userservice/dto/UserResponse.java
@@ -0,0 +1,15 @@
+package com.fitness.userservice.dto;
+
+import java.time.LocalDateTime;
+
+
+public record UserResponse(
+        String id,
+        String email,
+        String password,
+        String firstName,
+        String lastName,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt) {
+
+}
