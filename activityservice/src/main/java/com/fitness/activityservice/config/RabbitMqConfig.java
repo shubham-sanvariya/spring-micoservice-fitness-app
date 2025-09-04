375d5746157df5fc4caf0c72fdcf87c957196c0c
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-09-04 22:27:27 +0530

    refactor RabbitMqConfig to use properties for exchange, queue, and routing key

--- activityservice/src/main/java/com/fitness/activityservice/config/RabbitMqConfig.java
+++ activityservice/src/main/java/com/fitness/activityservice/config/RabbitMqConfig.java
@@ -1,8 +1,12 @@
 package com.fitness.activityservice.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,12 +14,30 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfig {
-    
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.queue.name}")
+    private String queue;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
     @Bean
     Queue activityQueue(){
         // here the value is set to be true so that 
         // even when the rabbit mq restarts the messages remains there
-        return new Queue("activity.queue", true);
+        return new Queue(queue, true);
+    }
+
+    @Bean
+    DirectExchange activityExchange(){
+        return new DirectExchange(exchange);
+    }
+
+    @Bean
+    Binding activityBinding(Queue activityQueue, DirectExchange activityExchange){
+        return  BindingBuilder.bind(activityQueue).to(activityExchange).with(routingKey);
     }
 
     @Bean
